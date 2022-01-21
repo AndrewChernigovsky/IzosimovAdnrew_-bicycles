ed6a7b6c294cffa06a3ff6c3dadc3b473bb626e9
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-01-21 15:48:39 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -21,50 +21,26 @@ const
   mainForm = document.getElementById('mainForm'),
   userNameMainForm = document.getElementById('username'),
   userPhoneMainForm = document.getElementById('phone'),
-  inputs = document.querySelectorAll('.Mainform-calltoAction__input'),
   buttonMainForm = document.getElementById('mainFormSubmit');
 
   mainForm.addEventListener('submit', function(evt) {
         if (isValidName(userNameMainForm)){
-          userNameMainForm.classList.add('error');
-          evt.preventDefault();    
+          userNameMainForm.classList.add('error')
+          evt.preventDefault();
         } else {
           userNameMainForm.classList.remove('error')
           userNameMainForm.classList.add('success');
       }  
 
       if (isValidPhone(userPhoneMainForm)){
-        userPhoneMainForm.classList.add('error');
+        userPhoneMainForm.classList.add('error')
         evt.preventDefault();    
       } else {
         userPhoneMainForm.classList.remove('error')
         userPhoneMainForm.classList.add('success');
+        
     }  
   });
-
-  // userNameMainForm.addEventListener('change', function() {
-  //     if (isValidName(userNameMainForm)){
-  //       if(userNameMainForm.classList.contains('success')) {
-  //         userNameMainForm.classList.remove('success')
-  //         userNameMainForm.classList.add('error');
-  //       } else {
-  //         userNameMainForm.classList.remove('error');
-  //         userNameMainForm.classList.add('success');
-  //     }
-  //   };     
-  // })
-
-  // userPhoneMainForm.addEventListener('change', function() {
-  //     if (isValidPhone(userPhoneMainForm)){
-  //       if(userPhoneMainForm.classList.contains('success')) {
-  //         userPhoneMainForm.classList.remove('success')
-  //         userPhoneMainForm.classList.add('error');
-  //       } else {
-  //         userPhoneMainForm.classList.remove('error');
-  //         userPhoneMainForm.classList.add('success');
-  //     }
-  //   };
-  // })
 
   function isValidPhone(input){
     return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
@@ -73,3 +49,8 @@ const
   function isValidName(input){
     return !/^\D{1,3}[А-Яа-яA-Za-z0-9_-]{3,30}$/.test(input.value);
   };
+
+userPhoneMainForm.oninput = function(){
+  var rep = /[a-zA-ZА-Яа-я]/g; 
+  this.value = this.value.replace(rep, '');
+}
