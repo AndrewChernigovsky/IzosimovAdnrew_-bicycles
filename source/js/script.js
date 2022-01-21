08a529bd43e30d73f11f0ad9fdc09b72ec3c0396
Andrew Chernigovsky <chernigovsky108@gmail.com>
2022-01-21 15:04:13 +0300

    1

--- source/js/script.js
+++ source/js/script.js
@@ -25,66 +25,46 @@ const
   buttonMainForm = document.getElementById('mainFormSubmit');
 
   mainForm.addEventListener('submit', function(evt) {
-    inputs.forEach(el => {
-      if (el.nextElementSibling){
-        el.nextElementSibling.remove()
-      }
-  
-      if (el.value === ' ') {
-        el.classList.add('error');
-        el.parentElement.insertAdjacentHTML(
-          "beforeend",
-          `<span class="errorText">поле должно быть заполнено</span>`
-        );
-      evt.preventDefault();
-  
-      } else {
-        if (el.classList.contains('error')) {
-          el.classList.remove('error');
-          el.classList.add('success');
-        }
-      }
-    });
-  
-        if (isValidPhone(userPhoneMainForm)){
-          if (userPhoneMainForm.nextElementSibling){
-            userPhoneMainForm.nextElementSibling.remove()
-          }
-          userPhoneMainForm.classList.remove('success');
-          userPhoneMainForm.classList.add('error');
-          userPhoneMainForm.parentElement.insertAdjacentHTML(
-            "beforeend",
-            `<span class="errorText">Неккоректно заполнено</span>`
-          );
-          evt.preventDefault();
-        } else {
-          if (userPhoneMainForm.nextElementSibling){
-            userPhoneMainForm.nextElementSibling.remove()
-          }
-          userPhoneMainForm.classList.remove('error');
-          userPhoneMainForm.classList.add('success');
-        }
-  
         if (isValidName(userNameMainForm)){
-          if (userNameMainForm.nextElementSibling){
-            userNameMainForm.nextElementSibling.remove()
-          }
-          userNameMainForm.classList.remove('success');
           userNameMainForm.classList.add('error');
-          userNameMainForm.parentElement.insertAdjacentHTML(
-            "beforeend",
-            `<span class="errorText">Неккоректно заполнено</span>`
-          );
-          evt.preventDefault();
+          evt.preventDefault();    
         } else {
-          if (userNameMainForm.nextElementSibling){
-            userNameMainForm.nextElementSibling.remove()
-          }
-          userNameMainForm.classList.remove('error');
+          userNameMainForm.classList.remove('error')
           userNameMainForm.classList.add('success');
-        }
+      }  
+
+      if (isValidPhone(userPhoneMainForm)){
+        userPhoneMainForm.classList.add('error');
+        evt.preventDefault();    
+      } else {
+        userPhoneMainForm.classList.remove('error')
+        userPhoneMainForm.classList.add('success');
+    }  
   });
 
+  // userNameMainForm.addEventListener('change', function() {
+  //     if (isValidName(userNameMainForm)){
+  //       if(userNameMainForm.classList.contains('success')) {
+  //         userNameMainForm.classList.remove('success')
+  //         userNameMainForm.classList.add('error');
+  //       } else {
+  //         userNameMainForm.classList.remove('error');
+  //         userNameMainForm.classList.add('success');
+  //     }
+  //   };     
+  // })
+
+  // userPhoneMainForm.addEventListener('change', function() {
+  //     if (isValidPhone(userPhoneMainForm)){
+  //       if(userPhoneMainForm.classList.contains('success')) {
+  //         userPhoneMainForm.classList.remove('success')
+  //         userPhoneMainForm.classList.add('error');
+  //       } else {
+  //         userPhoneMainForm.classList.remove('error');
+  //         userPhoneMainForm.classList.add('success');
+  //     }
+  //   };
+  // })
 
   function isValidPhone(input){
     return !/^(\+7|7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$/.test(input.value);
